acc2609d6a8c8bf9a93a493b2074c9e5b3183f4e
absmugz <absmugz09@gmail.com>
2016-10-14 00:16:19 +0200

    medical aid template

---
+++ public/wp-content/themes/storefront/template-recipes.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * The template for displaying the homepage.
+ *
+ * This page template will display any functions hooked into the `homepage` action.
+ * By default this includes a variety of product displays and the page content itself. To change the order or toggle these components
+ * use the Homepage Control plugin.
+ * https://wordpress.org/plugins/homepage-control/
+ *
+ * Template name: Recipes
+ *
+ * @package storefront
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			
+			<?php 
+    query_posts(array( 
+        'post_type' => 'recipe',
+        'showposts' => 10 
+    ) );  
+?>
+<?php while (have_posts()) : the_post(); ?>
+        <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+        <p><?php echo get_the_excerpt(); ?></p>
+<?php endwhile;?> 
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+<?php
+get_footer();
