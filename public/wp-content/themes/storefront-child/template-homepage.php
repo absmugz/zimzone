c1cd55e1559f90f4f11d25f28cd40787f0f8df7f
absmugz <absmugz09@gmail.com>
2016-10-19 02:05:19 +0200

    fixing content and templates

---
+++ public/wp-content/themes/storefront-child/template-homepage.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The template for displaying the homepage.
+ *
+ * This page template will display any functions hooked into the `homepage` action.
+ * By default this includes a variety of product displays and the page content itself. To change the order or toggle these components
+ * use the Homepage Control plugin.
+ * https://wordpress.org/plugins/homepage-control/
+ *
+ * Template name: Homepage
+ *
+ * @package storefront
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			
+			<?php
+
+echo do_shortcode('[products ids="60, 67, 70, 73, 76, 79"]');
+
+?>
+			
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+<?php
+get_footer();
